ee7619d0d4c6e4d48e59399f24776fe2155a5ab1
nicolasvandooren <vandooren.nicolas@gmail.com>
2018-09-07 14:01:47 +0200

    Update of script bash and the part of launching Kheops done

---
+++ AnsibleAutomatisation/checkSSH.sh
@@ -0,0 +1,5 @@
+ssh -q vandooni@172.23.63.29
+echo $?
+ssh vandooni@172.23.63.29
+
+echo $HOME
--- AnsibleAutomatisation/run.sh
+++ AnsibleAutomatisation/run.sh
@@ -1,9 +1,28 @@
 echo 'Enter the hosts file'
 read hosts
-echo 'Enter the playbook file'
-#read playbook
 
-playbook=mainInstall.yml
+hosts=hosts
+
+ssh -q vandooni@172.23.63.29 exit
+RESULT=$?
+if [ $RESULT -ne 0 ]
+then
+  echo 'SSH connection impossible'
+  ssh vandooni@172.23.63.29
+  echo 'Do you make a keygen ? (y/n)'
+  read RESPONSE
+  RESPONSE=${RESPONSE,,}
+  if [[ $RESPONSE =~ ^(yes|y) ]]
+  then
+    ssh-keygen -f $HOME"/.ssh/known_hosts" -R 172.23.63.29
+    ssh-keyscan -H 172.23.63.29 >> ~/.ssh/known_hosts
+  fi
+fi
+
+echo 'Enter the playbook file'
+read playbook
+
+playbook=main.yml
 
 echo 'Check the syntax of the playbook'
 ansible-playbook $playbook --syntax-check
